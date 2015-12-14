cd8c3a926ea2bd87ac4c8f13310bc0ae3946973f
Felipe Quintero <ffquint@gmail.com>
2015-12-14 14:44:43 -0800

    Generated Devise controllers for Users, updated code in Sessions controller to redirect users to their 'show' view after sign-in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'about' => 'welcome#about'
-  
+
   root to: 'welcome#index'
 
-  devise_for :users
+  devise_for :users, :controllers => { :sessions => "users/sessions" }
   resources :users
 
 end
