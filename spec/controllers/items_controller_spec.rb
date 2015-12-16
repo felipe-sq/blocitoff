e86dc57388186bad1637f73d716af5ffe27acc35
Felipe Quintero <ffquint@gmail.com>
2015-12-16 11:58:32 -0800

    Added more tests to items_controller_spec. Commented line 12 in users show view (temporary) to allow app to be viewed via rails server.

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -3,7 +3,7 @@ include RandomData
 
 RSpec.describe ItemsController, type: :controller do
   describe '#create' do
-    let(:user) { create(:user) } 
+    let(:user) { create(:user) }
 
     context 'user signed in' do
       before { sign_in user }
@@ -13,19 +13,31 @@ RSpec.describe ItemsController, type: :controller do
           change { Item.count }.by(1)
       end
 
-      it 'redirects to user#show'
+      it 'redirects to user#show' do
+        post :create, user_id: user.id, item: {name: 'something'}
+        expect(response).to redirect_to user
+      end
 
-      it 'should belong to the user'
+      it 'should belong to the user' do
+        post :create, user_id: user.id, item: {name: 'something'}
+        expect(assigns(:item)).to eq(user)
+      end
 
 
 
       context 'blank item' do
-        it 'displays an error'
+        it 'displays an error' do
+          post :create, user_id: user.id, item: {name: 'something'}
+          expect(:item).not_to be_nil
+        end
       end
     end
 
     context 'user not signed in' do
-      it 'does not create an item'
+      it 'does not create an item' do
+        post :create, user_id: user.id, item: {name: 'something'}
+        expect(response).to redirect_to(new_session_path)
+      end
     end
   end
 end
