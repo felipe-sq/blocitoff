c66bf1ec8b11dab9638e76c64828c0ccf7b76f82
Felipe Quintero <ffquint@gmail.com>
2015-12-20 14:14:47 -0800

    Added code to print response body in rspec test output.

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe ItemsController, type: :controller do
 
       it 'should belong to the user' do
         post :create, user_id: user.id, item: { user_id: user.id, name: 'something' }
-        p response.status
+        p response.body
         expect(assigns(item: :user_id)).to eq(user.id)
       end
 
